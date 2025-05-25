1981172a26c259d660c5086688de526f240552fa
Aheguy4ik <matveiluzkov@inbox.ru>
2025-05-25 16:50:20 +0300

    Build

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/aheguy/laba05
+# Build directory: /home/aheguy/laba05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/aheguy/laba05/_build/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/aheguy/laba05/CMakeLists.txt;37;add_test;/home/aheguy/laba05/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
