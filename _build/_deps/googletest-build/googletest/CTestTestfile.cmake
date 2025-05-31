b16ac5d0817993bbc9dcfadbe2f4e906978e2774
Aheguy4ik <matveiluzkov@inbox.ru>
2025-05-31 11:57:18 +0300

    add .gitignore + correct mock

--- _build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aheguy/laba05/_build/_deps/googletest-src/googletest
-# Build directory: /home/aheguy/laba05/_build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aheguy/laba05
-# Build directory: /home/aheguy/laba05/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/aheguy/laba05/build/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/aheguy/laba05/CMakeLists.txt;37;add_test;/home/aheguy/laba05/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
